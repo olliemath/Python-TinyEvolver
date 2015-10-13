3731c839fb718a2a4272bc04a05e92018ebc66e1
Oliver Margetts <oliver.margetts@gmail.com>
2015-10-13 18:02:53 +0100

    Check for setuptools.

--- setup.py
+++ setup.py
@@ -1,11 +1,15 @@
 # Always prefer setuptools over distutils
-from setuptools import setup, find_packages
 from os import path
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    print("warning: you do not have setuptools installed - cannot use \"develop\" option")
+    from disutils.core import setup
 
 here = path.abspath(path.dirname(__file__))
 
 # Get the long description from the README file
-with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+with open(path.join(here, 'README.md'), 'r') as f:
     long_description = f.read()
 
 setup(
